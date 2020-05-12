c7f80f0e405bda13b8d09682718790bc37b17e97
Ahmed KHABER <Ahmed.gsm1983@gmail.com>
2020-05-12 19:16:55 +0100

    Create register HTML form

--- index.php
+++ index.php
@@ -1,39 +1,20 @@
-<?php
-// 3.1.9 - Check session variables
-//   Support VirtualHost IDNA ServerName
-// echo 'Hello new PHP!<br>';
-$TABLE_NAME = "users";
+ <!-- This form used for debugging purposes -->
+ <form method="post" action="insert.php">
+  <label for="name" >Full name:</label><br>
+  <input type="text" id="name" name="name"><br>
+  
+  <label for="email">Email:</label><br>
+  <input type="text" id="email" name="email"><br>
+  
+  <label for="password">Password:</label><br>
+  <input type="text" id="password" name="password"><br>
+  
+  <label for="age">Age:</label><br>
+  <input type="text" id="age" name="age"><br>
+  
+  <label for="occupation">Occupation:</label><br>
+  <input type="text" id="occupation" name="occupation"><br>
+  
+  <input type="submit" text="submit" ><br>
+</form> 
 
-$COLUMN_NAME= "name";
-$COLUMN_ADDRESS= "address";
-$COLUMN_AGE= "age";
-$COLUMN_TELEPHONE= "telephone";
-
-try
-{
-$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-}
-catch(Exception $e)
-{
-die('Erreur : '.$e->getMessage());
-}
-
-// Insert new line in database
-$name = "Ahmed";
-$telephone = "0552 09 79 64";
-$address = "Beni Hamad, Mechedallah, BOUIRA, Algeria";
-$age = 37;
-
-// Insert data lines to database
-$req = $bdd->prepare("INSERT INTO $TABLE_NAME($COLUMN_NAME, $COLUMN_ADDRESS, $COLUMN_AGE, $COLUMN_TELEPHONE)
-VALUES (:$COLUMN_NAME, :$COLUMN_ADDRESS, :$COLUMN_AGE ,:$COLUMN_TELEPHONE )");
-$valueArray = array($COLUMN_NAME => $name,
-$COLUMN_ADDRESS => $address,
-$COLUMN_AGE => $age,
-$COLUMN_TELEPHONE => $telephone,
-);
-$req->execute($valueArray);
-
-
-
-?>
